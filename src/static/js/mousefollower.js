322a1afb3e1f3491fc9d653bb66141eabe6dfb0a
Elliot <captainexpo@gmail.com>
2025-03-15 14:13:15 -0400

    Moving

--- src/static/js/mousefollower.js
+++ src/static/js/mousefollower.js
@@ -1,28 +1,67 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const floatingImage = document.getElementById("floating-image");
-    let mouseX = window.innerWidth/2;
-    let mouseY = window.innerHeight/2;
-    let imgX = mouseX/2;
-    let imgY = mouseY/2;
+document.addEventListener("DOMContentLoaded", async function () {
+    let mouseX = window.innerWidth / 2;
+    let mouseY = window.innerHeight / 2;
     const speed = 0.05; // Adjust speed (lower is slower)
+
+    for (let i = 0; i < 100; i++)
+        document.body.insertAdjacentHTML(
+            "beforeend",
+            `<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="${await getRandomAd()}" vx="1" vy="1" px="${Math.random() * 1000}" py="${Math.random() * 1000}" class="follow-ad"></a>`,
+        );
 
     document.addEventListener("mousemove", (event) => {
         mouseX = event.clientX;
         mouseY = event.clientY;
     });
 
-    function moveImage() {
+    const mouseMass = 5;
+
+    function moveImage(image) {
         // Calculate movement with easing
-        imgX += (mouseX - imgX) * speed;
-        imgY += (mouseY - imgY) * speed;
+        let imgX = parseFloat(image.getAttribute("px"));
+        let imgY = parseFloat(image.getAttribute("py"));
+        let vx = parseFloat(image.getAttribute("vx"));
+        let vy = parseFloat(image.getAttribute("vy"));
+
+        const vel =
+            speed *
+            (mouseMass /
+                Math.sqrt((mouseX - imgX) ** 2 + (mouseY - imgY) ** 2));
+        vx += (mouseX - imgX) * vel;
+        vy += (mouseY - imgY) * vel;
+
+        imgX += vx;
+        imgY += vy;
+
+        image.setAttribute("px", imgX);
+        image.setAttribute("py", imgY);
+        image.setAttribute("vx", vx);
+        image.setAttribute("vy", vy);
+
+        // Boundary collision
+        if (imgX < 0 || imgX > window.innerWidth) {
+            vx = -0.75 * vx;
+            image.setAttribute("vx", vx);
+        }
+
+        if (imgY < 0 || imgY > window.innerHeight) {
+            vy = -0.75 * vy;
+
+            image.setAttribute("vy", vy);
+        }
 
         // Apply new position
-        floatingImage.style.left = `${imgX - 100}px`;
-        floatingImage.style.top = `${imgY - 50}px`;
-
-        requestAnimationFrame(moveImage);
+        image.style.left = `${imgX - 100}px`;
+        image.style.top = `${imgY - 50}px`;
     }
 
-    moveImage();
-});
+    function update() {
+        const floatingImage = document.querySelectorAll(".follow-ad");
+        floatingImage.forEach((image) => {
+            moveImage(image);
+        });
+        requestAnimationFrame(update);
+    }
 
+    update();
+});
